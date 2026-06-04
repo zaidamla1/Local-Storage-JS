e7ace3009b82e70077034f02312503f2f0ef1ac9
zaid <kkzyxw@gmail.com>
2026-06-04 09:13:39 +0530

    Calc function added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-localStorage.setItem("username","zaid_amla")
+localStorage.setItem("username", "zaid_amla")
+function addNumbers(...nums) {
+    let sum = 0;
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+    }
+    console.log(sum)
+}
+
+addNumbers(5, 10, 15)
